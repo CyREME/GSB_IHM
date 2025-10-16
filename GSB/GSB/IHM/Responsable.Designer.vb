6d2ae1e19c9ce13aa6679444ee0fb3f70b9ae812
Emeric CELLIER <ecellier@estran-brest.education>
2025-10-17 01:57:05 +0200

    Test Nouveau Tableau

--- GSB/GSB/IHM/Responsable.Designer.vb
+++ GSB/GSB/IHM/Responsable.Designer.vb
@@ -22,9 +22,38 @@ Partial Class Responsable
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Responsable"
+        TableLayoutPanel1 = New TableLayoutPanel()
+        SuspendLayout()
+        ' 
+        ' TableLayoutPanel1
+        ' 
+        TableLayoutPanel1.ColumnCount = 7
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 14.28571F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 14.2857161F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 14.2857161F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 14.2857161F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 14.2857161F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 14.2857161F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 14.2857161F))
+        TableLayoutPanel1.Location = New Point(342, 155)
+        TableLayoutPanel1.Name = "TableLayoutPanel1"
+        TableLayoutPanel1.RowCount = 3
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Absolute, 150F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Absolute, 150F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Absolute, 150F))
+        TableLayoutPanel1.Size = New Size(764, 557)
+        TableLayoutPanel1.TabIndex = 0
+        ' 
+        ' Responsable
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1178, 786)
+        Controls.Add(TableLayoutPanel1)
+        Name = "Responsable"
+        Text = "Responsable"
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
 End Class
