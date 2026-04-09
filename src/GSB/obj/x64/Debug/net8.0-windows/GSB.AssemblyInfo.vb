cb549cc03c06e2eaeb7564ed1ad863314c54792c
Sacha RUAULT <sruault@estran-brest.education>
2026-04-09 12:27:36 +0200

    a

--- src/GSB/obj/x64/Debug/net8.0-windows/GSB.AssemblyInfo.vb
+++ src/GSB/obj/x64/Debug/net8.0-windows/GSB.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("GSB"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+346f761b8218122aaed5402a310b9c3a67210ddd"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8681ec723fb5e285a95e6b69690cbbaf2d4e883"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("GSB"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("GSB"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
